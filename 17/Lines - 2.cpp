7850e0d1d732d8d5151d2a87ca4bf00749cf8274
Rida <dz.swimmer@mail.ru>
2024-10-12 20:58:06 +0300

    Update Lines - 2.cpp

--- 17/Lines - 2.cpp
+++ 17/Lines - 2.cpp
@@ -3,55 +3,45 @@
 #include <string>
 #include <algorithm>
 #include <cmath>
+#include <queue>
 
 using namespace std;
 using ll = long long;
 
-int n,k, k1, l=0, an=-1;
-const int N = 250;
-char a[N][N], b[N][N];
-bool used[N][N];
-int dr[] = {-1, 0, 1, 0}, dc[] = { 0, 1, 0, -1 };
+const int N = 260;
+const int dc[] = { 0, 0, 1, -1 };
+const int dr[] = { 1, -1, 0, 0 };
 
-void dfc(int r, int c) {
-    if (a[r][c] == '@') {
-        if (an < 0 || (an > l && an >= 0)) {
-              an = l;
-            for (int i = 0; i < n; ++i) for (int j = 0; j < n; ++j) {
-                if (used[i][j] == 1) b[i][j] = '+';
-                else b[i][j] = a[i][j];
-            }
-            return;
-        }
-    }
-    used[r][c] = 1;
-    int r1 = r, c1 = c;
-    for (int i = 0; i < 4; ++i) {
-        r = r1 + dr[i];
-        c = c1 + dc[i];
-        if ((r>-1 && r<n) && (c>-1 && c<n) && a[r][c]=='.' && !used[r][c]) {
-            l++;
-            dfc(r, c);
-            l--;
+int n, d[N];
+
+string g[N][N];
+
+void bfs() {
+    memset(d, -1, sizeof d);
+    d[1] = 0;
+    queue<int>q;
+    q.push(1);
+    while (q.size() > 0) {
+        int u = q.front();
+        q.pop();
+        for(int v=0; v<n && g[u][v]!=""; ++v) if (d[v] == -1 && g[u][v] == ".") {
+            d[v] = d[v - 1] + 1;
+            q.push(v);
         }
     }
 }
+
 int main() {
     cin >> n;
-    for (int i = 0; i < n; ++i) for (int j = 0; j < n; ++j){
-        cin >> a[i][j];
-        if (a[i][j] == 'X') {
-            k = i;
-            k1 = j;
+    for (int i = 0; i < n; ++i) for (int j = 0; j < n; ++j) cin >> g[i][j];
+    bfs();
+    int u = n;
+    while (u != 1) {
+        for (int v = 0; v < n && g[u][v] != ""; ++v) if (d[v] == d[u] + 1) {
+            g[u][v] = "+";
+            u = v;
         }
-    }
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            cout << b[i][j];
-        }
-        cout << "\n";
-    }
 
-    
-
+        for (int i = 0; i < n; ++i) for (int j = 0; j < n; ++j) cout << g[i][j];
+    }
 }
